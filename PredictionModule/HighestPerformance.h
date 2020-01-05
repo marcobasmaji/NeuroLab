42209622a858fc7dc1c0e5904445af265481173a
kitgitter <57494175+kitgitter@users.noreply.github.com>
2020-01-05 15:08:34 +0100

    Add files via upload
    First commit created the classes without the implementation yet

---
+++ PredictionModule/HighestPerformance.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Mode.h"
+class HighestPerformance :
+	public Mode
+{
+};
+
---
+++ PredictionModule/NeuroPrediction.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Prediction.h"
+class NeuroPrediction :
+	public Prediction
+{
+};
+
