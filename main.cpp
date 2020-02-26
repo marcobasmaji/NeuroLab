1b0f36d6ef2cb67fde25d485d0dbe44d07024bf3
Jens21 <58881271+Jens21@users.noreply.github.com>
2020-02-26 23:25:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,20 @@
 #include <iostream>
 using namespace std;
 
+//#define TEST_PROJECT    //comment this line out to start the tests
+
+#ifdef TEST_PROJECT
+#include "Tests/TestStarter.hpp"
+#endif
+
 int main(int argc, char *argv[]) {
-  MasterController masterController;
+
+    #ifdef TEST_PROJECT
+        TestStarter *testStarter=new TestStarter(argc,argv);
+        return testStarter->runAllTests();
+    #else
+        MasterController masterController;
+    #endif
 
    //NeuroLabNet nn;
    //nn.trainWithMnist();
