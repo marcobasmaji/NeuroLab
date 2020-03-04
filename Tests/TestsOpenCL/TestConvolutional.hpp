4f78f4837ce8aa54c3f86ad6e3d6130466e1fdcf
Jens21 <58881271+Jens21@users.noreply.github.com>
2020-03-04 12:35:57 +0100

    Create TestConvolutional.hpp

---
+++ Tests/TestsOpenCL/TestConvolutional.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+#include "Tests/TestsOpenCL/Util.hpp"
+#include "Tools/opennn/files/opennn.h"
+#include "HardwareModule/OpenCL/OpenCLLayer.hpp"
+#include "HardwareModule/OpenCL/OpenCLLayerCreator.hpp"
+#include "HardwareModule/OpenCL/HardwareEnvironment.hpp"
+#include "HardwareModule/OpenCL/OpenCLEnvironmentCreator.hpp"
+#include "HardwareModule/OpenCL/HardwareType.cpp"
+
+using namespace testing;
+using namespace OpenNN;
+
+class ConvLayerCont{
+public:
+    int batchSize;
+    int inputMaps;
+    int inputHeight;
+    int outputMaps;
+    int outputHeight;
+    int kernelHeight;
+    int strideHeight;
+    int inputWidth;
+    int outputWidth;
+    int kernelWidth;
+    int strideWidth;
+    float*inputValues;
+    float*outputErrors;
+    float*weights;
+    float*biases;
+    float learningRate;
+
+    ~ConvLayerCont(){
+        delete[] inputValues;
+        delete[] outputErrors;
+        delete[] biases;
+        delete[] weights;
+    }
+};
+
+class TestConvolutional{
+public:
+    Util* util=new Util();
+
+    void createRandomConvLayerCont(ConvLayerCont* convLayer);
+    void createConvLayerOpennn(ConvLayerCont* convLayerCont, ConvolutionalLayer* convLayerOpennn);
+
+    int testFeedForward();
+    int testErrorComp();
+};
