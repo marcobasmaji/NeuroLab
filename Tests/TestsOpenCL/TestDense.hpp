ee754b1a3c2ab1caa8ecf3ef30c130962daa8438
Jens21 <58881271+Jens21@users.noreply.github.com>
2020-03-04 12:35:22 +0100

    Create TestDense.hpp

---
+++ Tests/TestsOpenCL/TestDense.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+#include "Tests/TestsOpenCL/Util.hpp"
+#include "Tools/opennn/files/opennn.h"
+#include "HardwareModule/OpenCL/OpenCLLayer.hpp"
+#include "HardwareModule/OpenCL/OpenCLLayerCreator.hpp"
+#include "HardwareModule/OpenCL/HardwareEnvironment.hpp"
+#include "HardwareModule/OpenCL/OpenCLEnvironmentCreator.hpp"
+#include "HardwareModule/OpenCL/HardwareType.cpp"
+
+using namespace testing;
+using namespace OpenNN;
+
+class DenseLayerCont{
+public:
+    int inputMaps;
+    int inputHeight;
+    int inputWidth;
+    int outputNeurons;
+    int batchSize;
+    float* biases;
+    float* weights;
+    float*inputValues;
+    float*outputErrors;
+    float learningRate;
+
+    ~DenseLayerCont(){
+        delete[]biases;
+        delete[]weights;
+        delete[]inputValues;
+        delete[]outputErrors;
+    }
+};
+
+class TestDense{
+public:
+    Util* util=new Util();
+
+    void createRandomDenseLayerCont(DenseLayerCont* denseLayerCont);
+    void createPerzeptronLayerOpennn(DenseLayerCont* denseLayerCont, PerceptronLayer* perceptronLayerOpennn);
+
+    int testFeedForward();
+    int testErrorComp();
+};
