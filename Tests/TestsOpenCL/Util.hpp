42b3ff96f9b3d9722e695ee0566a43ea71b06c21
Jens21 <58881271+Jens21@users.noreply.github.com>
2020-03-09 23:45:54 +0100

    Update Util.hpp

--- Tests/TestsOpenCL/Util.hpp
+++ Tests/TestsOpenCL/Util.hpp
@@ -6,12 +6,20 @@
 #include "HardwareModule/OpenCL/OpenCLEnvironment.hpp"
 #include "HardwareModule/OpenCL/HardwareType.cpp"
 
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Util
 {
 public:
     Util();
     void createRandomValues(float* array, int length);
     int getRandomIntValue(int fromValue, int toValue);
+    void loadStringToArray(string dir, vector<float>*data);
+    void convertVectorToFloatArray(vector<float>*vectorData, float*array);
+    string loadFile(string dir);
 };
 
 #endif // UTIL_HPP
