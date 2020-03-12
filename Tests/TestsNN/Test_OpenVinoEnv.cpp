73615b597998fe870985723c571468d4a185d7f0
mobasmaji <m.basmaji@gmail.com>
2020-03-12 11:18:52 +0100

    Corrected Test

--- Tests/TestsNN/Test_OpenVinoEnv.cpp
+++ Tests/TestsNN/Test_OpenVinoEnv.cpp
@@ -7,13 +7,16 @@ TEST(OpenVino, CombinationOfHardware)
     OpenVinoEnv env;
     env.setDistribution({{"CPU",15},{"MYRIAD",12}});
     vector<pair<string,int>> distribtuion = env.getDistribution();
+
     EXPECT_EQ(2,distribtuion.size());
-    EXPECT_EQ("MULTI:CPU,MYRIAD",distribtuion.back().first);
-    EXPECT_EQ(13,distribtuion.back().second);
-    EXPECT_EQ(3,distribtuion.front().second);
-    EXPECT_EQ("CPU",distribtuion.front().first);
+    EXPECT_EQ("MULTI:CPU,MYRIAD",distribtuion.front().first);
+    EXPECT_EQ(24,distribtuion.front().second);
+    EXPECT_EQ(3,distribtuion.back().second);
+    EXPECT_EQ("CPU",distribtuion.back().first);
 
     env.setDistribution({{"CPU",22}});
+    distribtuion = env.getDistribution();
+
     EXPECT_EQ(1,distribtuion.size());
     EXPECT_EQ(distribtuion.front().first,distribtuion.back().first);
     EXPECT_EQ("CPU",distribtuion.back().first);
