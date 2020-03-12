f05a25b5c24fdbcdde4e32e794847d2cbb64cb21
kitgitter <57494175+kitgitter@users.noreply.github.com>
2020-03-12 14:33:27 +0100

    Update PredictionController.cpp

--- ControllerModule/PredictionController.cpp
+++ ControllerModule/PredictionController.cpp
@@ -6,11 +6,11 @@
 /**
  * @brief basically creates the desired prediction class and acts as a 
  * 
- * @param availableHardware 
- * @param Operationmode 
- * @param numberOfImages 
- * @param NN 
- * @return DataResults 
+ * @param availableHardware a vector of strings containing the names of the hardware elements
+ * @param Operationmode a string containing the operation mode
+ * @param numberOfImages an integer that contains the number of images that need to be classified
+ * @param NN a string containing the name of the nn we are using
+ * @return DataResults a resulsts that contains the important facts about the prediciton
  */
 DataResults PredictionController::calculatePrediction(int numberOfImages,std::string NN, std::string Operationmode,std::vector<std::string>& availableHardware)
 {
