1b39ccb06fd2816dd891a0a4f98810303caae702
Jens21 <58881271+Jens21@users.noreply.github.com>
2020-03-20 12:53:27 +0100

    Update WeightsAndBiasesCreator.cpp

--- DataModule/WeightsAndBiasesCreator.cpp
+++ DataModule/WeightsAndBiasesCreator.cpp
@@ -1,4 +1,4 @@
-#include "WeightsAndBiasesCreator.h"
+#include "DataModule/WeightsAndBiasesCreator.h"
 #include <QDebug>
 
 WeightsAndBiasesCreator::WeightsAndBiasesCreator(int outputMaps, int inputMaps, int kernelHeight, int kernelWidth, int seed) {
@@ -8,6 +8,8 @@ WeightsAndBiasesCreator::WeightsAndBiasesCreator(int outputMaps, int inputMaps, 
         this->kernelWidth = kernelWidth;
         std::default_random_engine generator(seed);
 }
+
+WeightsAndBiasesCreator::WeightsAndBiasesCreator(){}
 
 vector<float> WeightsAndBiasesCreator::createWeights(){
     int inputNeurons = inputMaps * kernelHeight * kernelWidth;
@@ -19,7 +21,7 @@ vector<float> WeightsAndBiasesCreator::createWeights(){
             for(int kernelY=0; kernelY < kernelHeight; kernelY++){
                 for(int kernelX=0; kernelX < kernelWidth; kernelX++){
                     //https://hackernoon.com/how-to-initialize-weights-in-a-neural-net-so-it-performs-well-3e9302d4490f for formula for relu
-                    weights[calculateId(outputMap, inputMap, kernelY, kernelX)]=(float)(sqrt(2) * distribution(generator)/(float)inputNeurons);
+                    weights[calculateId(outputMap, inputMap, kernelY, kernelX)]=(float)(sqrt(2) * distribution(generator)/sqrt((float)inputNeurons));
                 }
             }
         }
@@ -55,62 +57,59 @@ void WeightsAndBiasesCreator::save(vector<float> values, string dir){
 }
 
 
-    int WeightsAndBiasesCreator::calculateWeightId(int inputMaps, int inputHeight, int inputWidth, int outputNeuron, int inputMap, int y, int x){
+int WeightsAndBiasesCreator::calculateWeightId(int inputMaps, int inputHeight, int inputWidth, int outputNeuron, int inputMap, int y, int x){
     return (((outputNeuron) * inputMaps + inputMap) * inputHeight + y) * inputWidth + x;
-    }
+}
 
-    int WeightsAndBiasesCreator::calculateId(int outputMap, int inputMap, int kernelY, int kernelX){
-        return calculateWeightId(inputMaps, kernelHeight, kernelWidth, outputMap, inputMap, kernelY, kernelX);
-    }
+int WeightsAndBiasesCreator::calculateId(int outputMap, int inputMap, int kernelY, int kernelX){
+    return calculateWeightId(inputMaps, kernelHeight, kernelWidth, outputMap, inputMap, kernelY, kernelX);
+}
 
-    string WeightsAndBiasesCreator::createWeightsFile(string fileName, string dir) {
-        qDebug()<<"weights file"<<endl;
-        vector<float> weights = createWeights();
-        fileName = dir + fileName;
-        save(weights, fileName);
-        return fileName;
-    }
+string WeightsAndBiasesCreator::createWeightsFile(string fileName, string dir) {
+    qDebug()<<"weights file"<<endl;
+    vector<float> weights = createWeights();
+    fileName = dir + fileName;
+    save(weights, fileName);
+    return fileName;
+}
 
-    void WeightsAndBiasesCreator::updateWeightsFile(string filePath, vector<vector<float>> weights, int size) {
-        for(int i = 0; i < size; i++){
-            save(weights[i], filePath);
-        }
+void WeightsAndBiasesCreator::updateWeightsFile(string filePath, vector<vector<float>> weights, int size) {
+    for(int i = 0; i < size; i++){
+        save(weights[i], filePath);
     }
+}
 
-    void WeightsAndBiasesCreator::updateWeightsFile(string filePath, vector<float> weights) {
-        save(weights, filePath);
-    }
+void WeightsAndBiasesCreator::updateWeightsFile(string filePath, vector<float> weights) {
+    save(weights, filePath);
+}
 
-    void WeightsAndBiasesCreator::createBiasFile(string fileName, string dir) {
-        vector<float> biases = createBias();
-        fileName = dir + fileName;
-        save(biases, fileName);
-    }
+void WeightsAndBiasesCreator::createBiasFile(string fileName, string dir) {
+    vector<float> biases = createBias();
+    fileName = dir + fileName;
+    save(biases, fileName);
+}
 
 
 /*
  * example of how to use the methods to create the files; please give the directory path in your computer;
  * the values for each layer should be correct
- *
- int main(int argc, char *argv[]) {
+ **/
+ void WeightsAndBiasesCreator::createWeightsAndBiases() {
 
      //conv1
     WeightsAndBiasesCreator  *creator;
     creator = new WeightsAndBiasesCreator(15, 3, 7, 7, 12345);
-    creator->createWeightsFile("conv1_weights", "/home/bianca/Documents/");
-    creator->createBiasFile("biases_conv1.txt", "/home/bianca/Documents/");
+    creator->createWeightsFile("conv1_weights.txt", "/home/jens/Schreibtisch/Weights/");
+    creator->createBiasFile("biases_conv1.txt", "/home/jens/Schreibtisch/Weights/");
 
     //conv2
     creator = new WeightsAndBiasesCreator(40, 15, 4, 4, 12347);
-    creator->createWeightsFile("weights_conv2.txt", "/home/bianca/Documents/");
-    creator->createBiasFile("biases_conv2.txt", "/home/bianca/Documents/");
+    creator->createWeightsFile("weights_conv2.txt", "/home/jens/Schreibtisch/Weights/");
+    creator->createBiasFile("biases_conv2.txt", "/home/jens/Schreibtisch/Weights/");
 
 
     //dense
     creator = new WeightsAndBiasesCreator(6, 40, 5, 5, 12346);
-    creator->createWeightsFile("weights_dense.txt", "/home/bianca/Documents/");
-    creator->createBiasFile("biases_dense.txt", "/home/bianca/Documents/");
-
-    return 0;
-    }
-*/
+    creator->createWeightsFile("weights_dense.txt", "/home/jens/Schreibtisch/Weights/");
+    creator->createBiasFile("biases_dense.txt", "/home/jens/Schreibtisch/Weights/");
+}
