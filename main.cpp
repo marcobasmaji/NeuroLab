86e3a85e56d4e3bbbf6eca4a40f4de22e4897750
Jens21 <58881271+Jens21@users.noreply.github.com>
2020-03-20 11:49:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,28 +4,31 @@
 #include <unistd.h>
 #include <iostream>
 using namespace std;
-#include "ControllerModule/MasterController.h"
-//#include "NNModule/NeuroLabNet.h"
-#include <ctime>
-#include <unistd.h>
-#include <iostream>
-using namespace std;
 
-//#define TEST_PROJECT    //comment this line out to start the tests
+//#define TEST_PROJECT      //comment this line out to start the tests
+#define CREATE_WEIGHTS    //commit this line out to create random weights and biases
 
 #ifdef TEST_PROJECT
 #include "Tests/TestStarter.hpp"
 #endif
 
+#ifdef CREATE_WEIGHTS
+#include "DataModule/WeightsAndBiasesCreator.h"
+#endif
+
+#include "DataModule/WeightsAndBiasesCreator.h"
+
 int main(int argc, char *argv[]) {
 
-    #ifdef TEST_PROJECT
+    #if defined(TEST_PROJECT)
         TestStarter *testStarter=new TestStarter(argc,argv);
         return testStarter->runAllTests();
+    #elif defined(CREATE_WEIGHTS)
+        WeightsAndBiasesCreator creator;
+        creator.createWeightsAndBiases();
     #else
         MasterController masterController;
     #endif
 
+    return 0;
 }
-
-
