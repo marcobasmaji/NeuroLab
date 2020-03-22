50607e4fa76e127cc2911bdecbacdf13cac44547
Jens21 <58881271+Jens21@users.noreply.github.com>
2020-03-22 18:27:35 +0100

    Update NeuroLabNet.h

--- NNModule/NeuroLabNet.h
+++ NNModule/NeuroLabNet.h
@@ -1,57 +1,53 @@
-#include <NNModule/NeuralNet.h>
-#include "Layers/ConvolutionLayer.h"
-#include "Layers/ReLULayer.h"
-#include "Layers/MaxPoolingLayer.h"
-#include "Layers/SoftmaxLayer.h"
-#include "Layers/DenseLayer.h"
-#include "Layers/Loss.h"
-#include <iostream>
+#pragma once
+
+#include "HardwareModule/OpenCL/HardwareEnvironment.hpp"
+#include "HardwareModule/OpenCL/OpenCLEnvironment.hpp"
+#include "HardwareModule/OpenCL/OpenCLEnvironmentCreator.hpp"
+#include "HardwareModule/OpenCL/OpenCLLayer.hpp"
+#include "HardwareModule/OpenCL/OpenCLLayerCreator.hpp"
+#include "NNModule/trainingItem.hpp"
+#include "DataModule/Result.h"
+
 #include <vector>
-#include <HardwareModule/OpenCL/OpenCLEnvironment.hpp>
-#include <HardwareModule/OpenCL/OpenCLEnvironmentCreator.hpp>
-#include "../HardwareModule/OpenCL/HardwareType.cpp"
-#include <DataModule/Result.h>
-#include <opencv2/opencv.hpp>
-#include <CL/cl.h>
-#include <fstream>
-#include <QDir>
+#include <string>
 
 using namespace std;
 
-class NeuroLabNet: public NeuralNet 
-{
-public:
-    NeuroLabNet();
-	void init();
-    vector<Result> classify();
-    void train(string weightsDir, string dataSetDir, string newWeightsDir);
-	void executeTransferLearning();
-    void updateDataSet(vector<string> dataSet);
-    vector<pair<string,float>> getLabelWithProb(float prob[]);
+class NeuroLabNet{
 private:
-    vector<string> dataSet;
-    OpenCLEnvironment* clEnv;
-    //Layers
+    OpenCLEnvironment* env;
+    OpenCLEnvironmentCreator* envCreator;
+    OpenCLLayerCreator* layerCreator;
+
     OpenCLLayer* conv1;
-    OpenCLLayer* conv2;
     OpenCLLayer* relu1;
-    OpenCLLayer* relu2;
     OpenCLLayer* max1;
+    OpenCLLayer* conv2;
+    OpenCLLayer* relu2;
     OpenCLLayer* max2;
     OpenCLLayer* dense;
-    OpenCLLayer* soft;
-    //loss function
-    Loss* lossFunction;
+    OpenCLLayer* softmax;
 
-    static bool sortBySec(const pair<int,int> &a,
-                  const pair<int,int> &b)
-    {
-        return (a.second > b.second);
-    }
+    vector<string> dataSet;
 
-    static bool sortinrev(const pair<string,float> &a,
-                   const pair<string,float> &b)
-    {
-           return (a.second > b.second);
-    }
+    void initNeuroLabNet();
+    void loadWeightsAndBiases(string weightsDir);
+    void saveWeightsAndBiases(string newWeightsDir);
+    void saveArray(float* array, int length, string dir);
+    void loadArray(float* array, int length, string dir);
+    void loadAllImagePaths(string dataSetDir);
+    vector<TrainingItem> getAllTrainingItems(string dataSetDir);
+    vector<TrainingItem> shuffleDataset(vector<TrainingItem>vec);
+    void calculateOutputErrors(int label, float* errors, float* outputs);
+    void loadImageToArray(float* inputValues, string path);
+    int predictedLabel(float*outputs);
+    vector<pair<string,float>> getLabelWithProb(float prob[]);
+
+public:
+    NeuroLabNet();
+    ~NeuroLabNet();
+
+    void train(string weightsDir, string dataSetDir, string newWeightsDir);
+    void updateDataSet(vector<string> dataSet);
+    vector<Result> classify();
 };
